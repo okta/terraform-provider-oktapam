89bfe37d24cc9714210f289977e347f23e071475
Sachin Saxena <103450525+sachinsaxena-okta@users.noreply.github.com>
2022-05-25 10:10:58 -0500

    Added examples for AD-Joined Functionality

---
+++ example/ad-joined/ad-joined-identities-project-override.tf
@@ -0,0 +1,27 @@
+// Create project with `forward_traffic` enabled & gateway selector
+resource "oktapam_project" "test-project" {
+  name                 = "project-one"
+  next_unix_uid        = 60120
+  next_unix_gid        = 63020
+  create_server_users  = true
+  ssh_certificate_type = "CERT_TYPE_ED25519_01"
+
+  // Forward through gateway, gateway must have matching label
+  forward_traffic  = true
+  gateway_selector = "env=test"
+}
+
+// We are not creating users in ASA. Provisioned from Okta tenant.
+// We can disable/delete provisioned users
+resource "oktapam_user" "test-user" {
+  name   = "test-user"
+  status = "ACTIVE" //Possible values ACTIVE / DISABLED / DELETED
+}
+
+resource "oktapam_project_user" "test-project-user" {
+  project_name = oktapam_project.test-project.name
+  user_name    = oktapam_user.test-user
+  //Override user attributes at project level
+  ad_identities              = ["dev@test.com", "qa@test.com"]
+  ad_passwordless_identities = ["dev@test.com", "qa@test.com"]
+}
