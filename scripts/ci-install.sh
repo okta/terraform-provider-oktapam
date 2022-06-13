3b3749ce2b2ce3006f9f59de1338117771aa6ba6
Wayne Earl <wayne.earl@okta.com>
2022-06-13 16:42:21 -0600

    move artifact copy to separate make target

--- scripts/ci-install.sh
+++ scripts/ci-install.sh
@@ -11,6 +11,8 @@ cd "${DIR}/.."
 make -f Makefile.ci ci-remove-container || echo "Container doesn't exist. Continue."
 # now do install
 make -f Makefile.ci ci-install
+# copy artifact to host
+make -f Makefile.ci ci-artifact-copy
 # remove container
 make -f Makefile.ci ci-remove-container
 # display contents of /tmp
