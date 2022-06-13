dd962c863f668ea0ca8d26403d9e408c3cd8770b
Wayne Earl <wayne.earl@okta.com>
2022-06-13 17:57:55 -0600

    change artifacts dir

--- scripts/ci-install.sh
+++ scripts/ci-install.sh
@@ -7,13 +7,13 @@ if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 
 cd "${DIR}/.."
 
+# create artifact directory
+mkdir artifacts
 # remove container first, if it happens to exist
 make -f Makefile.ci ci-remove-container || echo "Container doesn't exist. Continue."
-# now do install
-make -f Makefile.ci ci-install
-# copy artifact to host
+# now do install and copy artifact to host
 make -f Makefile.ci ci-artifact-copy
 # remove container
 make -f Makefile.ci ci-remove-container
-# display contents of /tmp
-ls -al /tmp
+# display contents of artifact dir
+ls -al artifacts
