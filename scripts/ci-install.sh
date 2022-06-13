9d22af148f037fa8918a32676bfdd73a4bd6e9d2
Wayne Earl <wayne.earl@okta.com>
2022-06-13 16:20:30 -0600

    change logic for export vars

--- scripts/ci-install.sh
+++ scripts/ci-install.sh
@@ -2,6 +2,10 @@
 
 set -eo pipefail
 
+export INSTALL_CONTAINER_NAME
+export INSTALL_TARGET
+export PROVIDER_BINARY
+
 DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 
@@ -12,7 +16,6 @@ make -f Makefile.ci ci-remove-container || echo "Container doesn't exist. Contin
 # now do install
 make -f Makefile.ci ci-install
 # copy artifact to host
-set -x
 docker cp "${INSTALL_CONTAINER_NAME}:${INSTALL_TARGET}/${PROVIDER_BINARY}" /tmp
 # remove container
 make -f Makefile.ci ci-remove-container
