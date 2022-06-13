374ebccdfea6150b2e061b4e1331b3165b4b1963
Wayne Earl <wayne.earl@okta.com>
2022-06-13 15:08:47 -0600

    add install target for ci make

---
+++ scripts/ci-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eo pipefail
+
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+
+cd "${DIR}/.."
+
+make -f Makefile.ci ci-install
+make -f Makefile.ci ci-remove-container
