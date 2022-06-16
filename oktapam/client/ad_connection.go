db5a1a146c4ee36342534114d3c85de687a900e6
Sachin Saxena <103450525+sachinsaxena-okta@users.noreply.github.com>
2022-06-15 22:52:14 -0500

    Create AD Connection and TaskSettings resources

---
+++ oktapam/client/ad_connection.go
@@ -0,0 +1,230 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+
+	"github.com/terraform-providers/terraform-provider-oktapam/oktapam/logging"
+	"github.com/terraform-providers/terraform-provider-oktapam/oktapam/utils"
+)
+
+type ADConnection struct {
+	Name                   *string  `json:"name"`
+	ID                     *string  `json:"id,omitempty"`
+	GatewayID              *string  `json:"gateway_id"`
+	Domain                 *string  `json:"domain"`
+	ServiceAccountUsername *string  `json:"service_account_username"`
+	ServiceAccountPassword *string  `json:"service_account_password"`
+	DomainControllers      []string `json:"domain_controllers,omitempty"`
+	UsePasswordless        *bool    `json:"use_passwordless"`
+	CertificateId          *string  `json:"certificate_id,omitempty"`
+	ActiveTaskSettingsId   *string  `json:"active_task_settings_id,omitempty"`
+	DeletedAt              *string  `json:"deleted_at,omitempty"`
+}
+
+type ADTaskSettings struct {
+	ID                         *string                  `json:"id,omitempty"`
+	Name                       *string                  `json:"name"`
+	Frequency                  *int                     `json:"frequency"`
+	StartHourUTC               *int                     `json:"start_hour_utc,omitempty"`
+	IsActive                   *bool                    `json:"is_active"`
+	RunTest                    *bool                    `json:"run_test"`
+	HostnameAttribute          *string                  `json:"hostname_attribute"`
+	AccessAddressAttribute     *string                  `json:"access_address_attribute,omitempty"`
+	OSAttribute                *string                  `json:"os_attribute"`
+	BastionAttribute           *string                  `json:"bastion_attribute,omitempty"`
+	AltNamesAttributes         []string                 `json:"alt_names_attributes,omitempty"`
+	AdditionalAttributeMapping []*ADAdditionalAttribute `json:"additional_attribute_mapping,omitempty"`
+	//RuleAssignments are not sorted in any order, consumers may want to sort by priority.
+	RuleAssignments []*ADRuleAssignment `json:"rule_assignments"`
+}
+
+type ADAdditionalAttribute struct {
+	Label  string `json:"label"`
+	Value  string `json:"value"`
+	IsGuid bool   `json:"is_guid"`
+}
+
+type ADRuleAssignment struct {
+	ID              string `json:"id"`
+	BaseDN          string `json:"base_dn"`
+	LDAPQueryFilter string `json:"ldap_query_filter"`
+	ProjectID       string `json:"project_id"`
+	ProjectName     string `json:"project_name"`
+	Priority        int    `json:"priority"`
+}
+
+type ADTaskSettingsSchedule struct {
+	Frequency    *int `json:"frequency"`
+	StartHourUTC *int `json:"startHourUTC"`
+}
+
+func (adConn ADConnection) Exists() bool {
+	return utils.IsNonEmpty(adConn.ID) && utils.IsBlank(adConn.DeletedAt)
+}
+
+func (adTaskSettings ADTaskSettings) Exists() bool {
+	return utils.IsNonEmpty(adTaskSettings.ID)
+}
+
+func (c OktaPAMClient) GetADConnection(ctx context.Context, id string) (*ADConnection, error) {
+	requestURL := fmt.Sprintf("/v1/teams/%s/integrations/ad_connections/%s", url.PathEscape(c.Team), url.PathEscape(id))
+	logging.Tracef("making GET request to %s", requestURL)
+	resp, err := c.CreateBaseRequest(ctx).SetResult(&ADConnection{}).Get(requestURL)
+	if err != nil {
+		logging.Errorf("received error while making request to %s", requestURL)
+		return nil, err
+	}
+	statusCode := resp.StatusCode()
+
+	if statusCode == 200 {
+		adConn := resp.Result().(*ADConnection)
+		if adConn.Exists() {
+			return adConn, nil
+		}
+		return nil, nil
+	} else if statusCode == 404 {
+		return nil, nil
+	}
+
+	return nil, createErrorForInvalidCode(resp, 200, 404)
+}
+
+func (c OktaPAMClient) CreateADConnection(ctx context.Context, adConn *ADConnection) error {
+	// Create the group on the api server specified by group
+	requestURL := fmt.Sprintf("/v1/teams/%s/integrations/ad_connections", url.PathEscape(c.Team))
+	logging.Tracef("making POST request to %s", requestURL)
+	resp, err := c.CreateBaseRequest(ctx).SetBody(adConn).SetResult(adConn).Post(requestURL)
+	if err != nil {
+		logging.Errorf("received error while making request to %s", requestURL)
+		return err
+	}
+
+	//if resp.StatusCode() == 201 {
+	//	adConn = resp.Result().(*ADConnection)
+	//}
+	_, err = checkStatusCode(resp, 201)
+	return err
+}
+
+func (c OktaPAMClient) UpdateADConnection(ctx context.Context, adConnId string, adConn *ADConnection) error {
+	requestURL := fmt.Sprintf("/v1/teams/%s/integrations/ad_connections/%s", url.PathEscape(c.Team), url.PathEscape(adConnId))
+	logging.Tracef("making PUT request to %s", requestURL)
+	resp, err := c.CreateBaseRequest(ctx).SetBody(*adConn).Put(requestURL)
+	if err != nil {
+		logging.Errorf("received error while making request to %s", requestURL)
+		return err
+	}
+	_, err = checkStatusCode(resp, 204)
+	return err
+}
+
+func (c OktaPAMClient) DeleteADConnection(ctx context.Context, adConnId string) error {
+	requestURL := fmt.Sprintf("/v1/teams/%s/integrations/ad_connections/%s", url.PathEscape(c.Team), url.PathEscape(adConnId))
+	logging.Tracef("making DELETE request to %s", requestURL)
+	resp, err := c.CreateBaseRequest(ctx).Delete(requestURL)
+	if err != nil {
+		logging.Errorf("received error while making request to %s", requestURL)
+		return err
+	}
+
+	_, err = checkStatusCode(resp, 204, 404)
+	return err
+}
+
+func (c OktaPAMClient) GetADTaskSettings(ctx context.Context, adConnId string, adTaskSettingsId string) (*ADTaskSettings, error) {
+	requestURL := fmt.Sprintf("/v1/teams/%s/integrations/ad_connections/%s/task_settings/%s", url.PathEscape(c.Team),
+		url.PathEscape(adConnId), url.PathEscape(adTaskSettingsId))
+	logging.Tracef("making GET request to %s", requestURL)
+	resp, err := c.CreateBaseRequest(ctx).SetResult(&ADTaskSettings{}).Get(requestURL)
+	if err != nil {
+		logging.Errorf("received error while making request to %s", requestURL)
+		return nil, err
+	}
+	statusCode := resp.StatusCode()
+
+	if statusCode == 200 {
+		adTaskSettings := resp.Result().(*ADTaskSettings)
+		if adTaskSettings.Exists() {
+			return adTaskSettings, nil
+		}
+		return nil, nil
+	} else if statusCode == 404 {
+		return nil, nil
+	}
+
+	return nil, createErrorForInvalidCode(resp, 200, 404)
+}
+
+func (c OktaPAMClient) CreateADTaskSettings(ctx context.Context, adConnId string, adTaskSettings *ADTaskSettings) error {
+	// Create the group on the api server specified by group
+	requestURL := fmt.Sprintf("/v1/teams/%s/integrations/ad_connections/%s/task_settings", url.PathEscape(c.Team),
+		url.PathEscape(adConnId))
+	logging.Tracef("making POST request to %s", requestURL)
+	resp, err := c.CreateBaseRequest(ctx).SetBody(adTaskSettings).SetResult(adTaskSettings).Post(requestURL)
+
+	if err != nil {
+		logging.Errorf("received error while making request to %s", requestURL)
+		return err
+	}
+	_, err = checkStatusCode(resp, 201)
+	return err
+}
+
+func (c OktaPAMClient) UpdateADTaskSettings(ctx context.Context, adConnId string, adTaskSettingsId string, adTaskSettings *ADTaskSettings) error {
+	requestURL := fmt.Sprintf("/v1/teams/%s/integrations/ad_connections/%s/task_settings/%s", url.PathEscape(c.Team),
+		url.PathEscape(adConnId), url.PathEscape(adTaskSettingsId))
+	logging.Tracef("making PUT request to %s", requestURL)
+	resp, err := c.CreateBaseRequest(ctx).SetBody(adTaskSettings).SetResult(adTaskSettings).Put(requestURL)
+
+	if err != nil {
+		logging.Errorf("received error while making request to %s", requestURL)
+		return err
+	}
+
+	_, err = checkStatusCode(resp, 204)
+	return err
+}
+
+func (c OktaPAMClient) DeleteADTaskSettings(ctx context.Context, adConnId string, adTaskSettingsId string) error {
+	requestURL := fmt.Sprintf("/v1/teams/%s/integrations/ad_connections/%s/task_settings/%s", url.PathEscape(c.Team),
+		url.PathEscape(adConnId), url.PathEscape(adTaskSettingsId))
+	logging.Tracef("making DELETE request to %s", requestURL)
+	resp, err := c.CreateBaseRequest(ctx).Delete(requestURL)
+	if err != nil {
+		logging.Errorf("received error while making request to %s", requestURL)
+		return err
+	}
+
+	_, err = checkStatusCode(resp, 204, 404)
+	return err
+}
+
+func (c OktaPAMClient) UpdateADTaskSettingsSchedule(ctx context.Context, adConnId string, adTaskSettingsId string, schedule ADTaskSettingsSchedule) error {
+	requestURL := fmt.Sprintf("/v1/teams/%s/integrations/ad_connections/%s/task_settings/%s/schedule", url.PathEscape(c.Team),
+		url.PathEscape(adConnId), url.PathEscape(adTaskSettingsId))
+	logging.Tracef("making POST request to %s", requestURL)
+	resp, err := c.CreateBaseRequest(ctx).SetBody(schedule).Post(requestURL)
+	if err != nil {
+		logging.Errorf("received error while making request to %s", requestURL)
+		return err
+	}
+
+	_, err = checkStatusCode(resp, 204)
+	return err
+}
+
+func (c OktaPAMClient) DeactivateADTaskSettings(ctx context.Context, adConnId string, adTaskSettingsId string) error {
+	requestURL := fmt.Sprintf("/v1/teams/%s/integrations/ad_connections/%s/task_settings/%s/deactivate", url.PathEscape(c.Team),
+		url.PathEscape(adConnId), url.PathEscape(adTaskSettingsId))
+	logging.Tracef("making POST request to %s", requestURL)
+	resp, err := c.CreateBaseRequest(ctx).Post(requestURL)
+	if err != nil {
+		logging.Errorf("received error while making request to %s", requestURL)
+		return err
+	}
+
+	_, err = checkStatusCode(resp, 204)
+	return err
+}
--- oktapam/utils/utils.go
+++ oktapam/utils/utils.go
@@ -1,5 +1,11 @@
 package utils
 
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
 func IsBlank(s *string) bool {
 	return s == nil || *s == ""
 }
@@ -40,4 +46,47 @@ func AsStringPtrZero(v string, returnZero bool) *string {
 		return nil
 	}
 	return &v
+}
+
+func ExpandStringSet(v *schema.Set) []string {
+	return ExpandStringList(v.List())
+}
+
+func ExpandStringList(vI []interface{}) []string {
+	vs := make([]string, len(vI))
+	for _, v := range vI {
+		val, ok := v.(string)
+		if ok && val != "" {
+			vs = append(vs, val)
+		}
+	}
+	return vs
+}
+
+func ConvertStringSliceToSet(strings []string) *schema.Set {
+	arr := make([]interface{}, len(strings))
+	for i, str := range strings {
+		arr[i] = str
+	}
+	return schema.NewSet(schema.HashString, arr)
+}
+
+func SetNonPrimitives(d *schema.ResourceData, valueMap map[string]interface{}) error {
+	for k, v := range valueMap {
+		if v != nil {
+			if err := d.Set(k, v); err != nil {
+				return fmt.Errorf("error setting %s for resource %s: %s", k, d.Id(), err)
+			}
+		}
+	}
+	return nil
+}
+
+func Contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
