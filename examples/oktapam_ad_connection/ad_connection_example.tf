3f843c16c336ea27d2495ec6f3a9d83d9963ccd8
Sachin Saxena <103450525+sachinsaxena-okta@users.noreply.github.com>
2022-06-27 12:32:03 -0500

    Create ad_connection_example.tf

---
+++ examples/oktapam_ad_connection/ad_connection_example.tf
@@ -0,0 +1,44 @@
+
+// Create project with `forward_traffic` enabled
+resource "oktapam_project" "test-project" {
+  name = "project-one"
+  next_unix_uid = 60120
+  next_unix_gid = 63020
+  create_server_users = true
+  ssh_certificate_type = "CERT_TYPE_ED25519_01"
+
+  // Forward through gateway, gateway must have matching label
+  forward_traffic = true
+  gateway_selector = "env=test"
+}
+
+//Active Directory Connection
+resource "oktapam_ad_connection" "test-ad-connection" {
+  name                     = "test-ad-connection"
+  gateway_id               = "<gateway-id>"
+  domain                   = "dev-test.test.com"
+  service_account_username = "ldap@dev-test"
+  service_account_password = "secret"
+  use_passwordless         = true
+  domain_controllers       = ["dc1.com", "dc2.com"] //Optional: DC used to query the domain
+}
+
+//AD Joined Server Discovery
+//Each Connection can have multiple server sync Jobs but only one active at a time
+// One option is to name it : oktapam_ad_connection_server_sync_job
+resource "oktapam_ad_task_settings" "test-ad-task-settings" {
+  connection_id            = oktapam_ad_connection.test-ad-connection.id
+  name                     = "daily-job-1"
+  is_active                = true
+  frequency                = 24 # Every 12 hours
+  host_name_attribute      = "dNSHostName"
+  access_address_attribute = "dNSHostName"
+  os_attribute             = "operatingSystem"
+  rule_assignments {
+    base_dn           = "ou=real,dc=dev-test,dc=sudo,dc=wtf"
+    ldap_query_filter = "(objectclass=computer)"
+    project_id        = oktapam_project.test-project.id
+    priority          = 1
+  }
+}
+
