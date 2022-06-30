81b2a955ae9ccb50ecb2f3d0422da5ce9c5a7287
Sachin Saxena <103450525+sachinsaxena-okta@users.noreply.github.com>
2022-06-30 11:19:57 -0500

    Fix for review comments

---
+++ oktapam/utils/utils_test.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestExpandStringList(t *testing.T) {
+	input := []interface{}{"foo", "bar", "foo"}
+	outputList := ExpandStringList(input)
+	expected := []string{
+		"foo",
+		"bar",
+		"foo",
+	}
+
+	if !reflect.DeepEqual(outputList, expected) {
+		t.Fatalf(
+			"Got:\n\n%#v\n\nExpected:\n\n%#v\n",
+			outputList,
+			expected)
+	}
+}
