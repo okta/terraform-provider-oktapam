ce1686d92dce9669cd83c364e7ff310723559f8f
Sachin Saxena <103450525+sachinsaxena-okta@users.noreply.github.com>
2022-08-12 11:37:18 -0500

    Fixed test

--- oktapam/data_source_server_enrollment_token.go
+++ oktapam/data_source_server_enrollment_token.go
@@ -67,9 +67,11 @@ func dataSourceServerEnrollmentTokenFetch(ctx context.Context, d *schema.Resourc
 	}
 
 	if token != nil {
-		d.SetId(createServerEnrollmentTokenResourceID(*token.Project, *token.ID))
+		d.SetId(*token.ID)
 		for key, value := range token.ToResourceMap() {
-			d.Set(key, value)
+			if err := d.Set(key, value); err != nil {
+				return diag.FromErr(err)
+			}
 		}
 	} else {
 		return diag.Errorf("%s %s does not exist", providerServerEnrollmentTokenKey, id)
