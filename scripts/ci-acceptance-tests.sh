76ac83b66b48096dc8986d336a63e961e36e61af
Andrew Hamilton <andrew.hamilton@okta.com>
2022-08-23 18:06:28 -0700

    Updates terraform binary provisioning
    - Updates the terraform bin setup script to pull down the correct
      version.
    - Provides the local terraform binary to the running Docker container
      with the integration tests.
    - Turns off usage of the terraform wrapper since we don't need it and
      adds extra dependencies.

--- scripts/ci-acceptance-tests.sh
+++ scripts/ci-acceptance-tests.sh
@@ -7,4 +7,12 @@ if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 
 cd "${DIR}/.."
 
+TF_BIN=$(which terraform)
+if [[ -x "${TF_BIN}" ]]; then
+    echo "found terraform at ${TF_BIN}, setting TF_ACC_TERRAFORM_PATH=${TF_BIN}"
+    # This path needs to be added to the Docker run command
+    export TF_ACC_TERRAFORM_PATH="${TF_BIN}"
+    export TF_ACC_TERRAFORM_VERSION="$(cat .terraform-version)"
+fi
+
 make -f Makefile.ci ci-acceptance-test
