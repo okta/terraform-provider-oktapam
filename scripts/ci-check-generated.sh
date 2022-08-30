952c287643cf0611ec97e2d4c15e14ed0d5ae627
Andrew Hamilton <andrew.hamilton@okta.com>
2022-08-29 17:18:03 -0700

    Adds .terraform-version for tag check
    - Currently the .terraform-version doesn't exist in the old tagged
      versions of the code so this part of the CI pipeline fails. If we
      checkout the lastest .terraform-version file we can test with out
      latest approved version of terraform.

--- scripts/ci-check-generated.sh
+++ scripts/ci-check-generated.sh
@@ -7,4 +7,11 @@ if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 
 cd "${DIR}/.."
 
+if [[ -x "${TF_BIN}" ]]; then
+    echo "found terraform at ${TF_BIN}, setting TF_ACC_TERRAFORM_PATH=${TF_BIN}"
+    # This path needs to be added to the Docker run command
+    export TF_ACC_TERRAFORM_PATH="${TF_BIN}"
+    export TF_ACC_TERRAFORM_VERSION="$(cat .terraform-version)"
+fi
+
 make -f Makefile.ci ci-check-generate
