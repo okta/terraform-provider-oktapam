c51a2422ff6cbcce4cbc7fc32bc6f5352867da04
SajanAlexander-okta <84344336+SajanAlexander-okta@users.noreply.github.com>
2023-12-07 12:09:58 -0500

    Apply suggestions from code review
    Co-authored-by: isaacdurham-okta <89145748+isaacdurham-okta@users.noreply.github.com>

--- oktapam/constants/descriptions/prefixes.go
+++ oktapam/constants/descriptions/prefixes.go
@@ -3,6 +3,6 @@ package descriptions
 const (
 	PrefixBeta = "Beta Feature:"
 	PrefixLEA  = "Limited Early Access Feature:"
-	PrefixDev  = "Under Development Feature :"
+	PrefixDev  = "Under Development Feature:"
 	PrefixGA   = "General Availability Feature:"
 )
