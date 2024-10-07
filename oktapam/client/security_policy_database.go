4710267e80cc0ba38e78ada3a0314faec85c49b5
JAmes Atwill <james.atwill@okta.com>
2024-10-07 00:39:24 -0700

    - interim commit

--- oktapam/client/security_policy_database.go
+++ oktapam/client/security_policy_database.go
@@ -3,102 +3,75 @@ package client
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/atko-pam/platform/backend/labels"
 	"github.com/okta/terraform-provider-oktapam/oktapam/constants/attributes"
 )
 
 type DatabaseBasedResourceSubSelectorType string
 
 const (
-	DatabaseBasedResourceSelectorType    = ResourceSelectorType("database_based_resource")
-	DatabaseLabelDatabaseSubSelectorType = DatabaseBasedResourceSubSelectorType("database_label")
+	DatabaseBasedResourceSelectorType     = ResourceSelectorType("database_based_resource")
+	DatabaseLabelsDatabaseSubSelectorType = DatabaseBasedResourceSubSelectorType("database_labels")
+	PasswordCheckoutDatabasePrivilegeType = PrivilegeType("password_checkout_database")
 )
 
-type DatabaseLabelDatabaseSelector struct {
-	Labels map[string]string `json:"labels"`
+// Privileges: password_checkout_database
+
+var _ SecurityPolicyRulePrivilege = &PasswordCheckoutDatabasePrivilege{}
+
+type PasswordCheckoutDatabasePrivilege struct {
+	Enabled *bool `json:"password_checkout_database"`
 }
 
-var _ DatabaseBasedResourceSubSelector = &DatabaseLabelBasedSubSelector{}
-
-type DatabaseLabelBasedSubSelector struct {
-	DatabaseSelector *DatabaseLabelDatabaseSelector `json:"database_selector"`
-
-	AccountSelectorType AccountSelectorType `json:"account_selector_type"`
-	AccountSelector     AccountSelector     `json:"account_selector"`
+func (p *PasswordCheckoutDatabasePrivilege) ValidForResourceType(resourceSelectorType ResourceSelectorType) bool {
+	return resourceSelectorType == DatabaseBasedResourceSelectorType
 }
 
-var _ SecurityPolicyRuleResourceSelector = &DatabaseBasedResourceSelector{}
-
-type DatabaseBasedResourceSelector struct {
-	Selectors []DatabaseBasedResourceSubSelectorContainer `json:"selectors"`
-}
-
-func (s *DatabaseBasedResourceSelector) ResourceSelectorType() ResourceSelectorType {
-	return DatabaseBasedResourceSelectorType
-}
-
-func (s *DatabaseBasedResourceSelector) ToResourceMap() map[string]any {
-	m := make(map[string]any, 1)
-
-	databasesArr := make([]any, 1)
-	subSelectorsMap := make(map[string]any, 3)
-	databasesArr[0] = subSelectorsMap
-
-	individualDatabasesArr := make([]any, 0, len(s.Selectors))
-	databaseLabelsArr := make([]any, 0, 1)
-
-	for _, subSelector := range s.Selectors {
-		switch subSelector.SelectorType {
-		case DatabaseLabelDatabaseSubSelectorType:
-			databaseLabelsArr = append(databaseLabelsArr, subSelector.Selector.ToResourceMap())
-		}
-	}
-
-	subSelectorsMap[attributes.Database] = individualDatabasesArr
-
-	m[attributes.Databases] = databasesArr
+func (p *PasswordCheckoutDatabasePrivilege) ToResourceMap() map[string]any {
+	m := make(map[string]any)
+	m[attributes.Enabled] = *p.Enabled
 	return m
 }
 
-func (*DatabaseLabelBasedSubSelector) DatabaseBasedResourceSubSelectorType() DatabaseBasedResourceSubSelectorType {
-	return DatabaseLabelDatabaseSubSelectorType
+// Sub-Selectors: database_labels
+
+type DatabaseLabelsDatabaseSelector struct {
+	Labels labels.Labels `json:"labels"`
 }
 
-func (dbLabelSubSelector *DatabaseLabelBasedSubSelector) ToResourceMap() map[string]any {
+var _ DatabaseBasedResourceSubSelector = &DatabaseLabelsBasedSubSelector{}
+
+type DatabaseLabelsBasedSubSelector struct {
+	DatabaseSelector *DatabaseLabelsDatabaseSelector `json:"database_selector"`
+}
+
+func (*DatabaseLabelsBasedSubSelector) DatabaseBasedResourceSubSelectorType() DatabaseBasedResourceSubSelectorType {
+	return DatabaseLabelsDatabaseSubSelectorType
+}
+
+func (d *DatabaseLabelsBasedSubSelector) ToResourceMap() map[string]any {
 	m := make(map[string]any)
 
-	databaseLabelsM := make(map[string]any, len(dbLabelSubSelector.DatabaseSelector.Labels))
-	for k, v := range dbLabelSubSelector.DatabaseSelector.Labels {
+	databaseLabelsM := make(map[string]any, len(d.DatabaseSelector.Labels))
+	for k, v := range d.DatabaseSelector.Labels {
 		databaseLabelsM[k] = v
 	}
 	m[attributes.DatabaseLabels] = databaseLabelsM
 
-	usernamesArr := make([]any, 0)
-
-	if dbLabelSubSelector.AccountSelectorType == UsernameAccountSelectorType {
-		usernamesArr = stringSliceToInterfaceSlice(dbLabelSubSelector.AccountSelector.(*UsernameAccountSelector).Usernames)
-	}
-
-	m[attributes.Accounts] = usernamesArr
-
 	return m
 }
 
-func (dbLabelSubSelector *DatabaseLabelBasedSubSelector) UnmarshalJSON(data []byte) error {
+func (d *DatabaseLabelsBasedSubSelector) UnmarshalJSON(data []byte) error {
 	tmp := struct {
-		DatabaseSelector *DatabaseLabelDatabaseSelector `json:"database_selector"`
+		DatabaseSelector *DatabaseLabelsDatabaseSelector `json:"database_selector"`
 	}{}
 
 	if err := json.Unmarshal(data, &tmp); err != nil {
 		return err
 	}
 
-	dbLabelSubSelector.DatabaseSelector = tmp.DatabaseSelector
+	d.DatabaseSelector = tmp.DatabaseSelector
 	return nil
-}
-
-type DatabaseBasedResourceSubSelector interface {
-	DatabaseBasedResourceSubSelectorType() DatabaseBasedResourceSubSelectorType
-	ToResourceMap() map[string]any
 }
 
 var _ json.Unmarshaler = &DatabaseBasedResourceSubSelectorContainer{}
@@ -120,8 +93,8 @@ func (c *DatabaseBasedResourceSubSelectorContainer) UnmarshalJSON(data []byte) e
 
 	c.SelectorType = tmp.SelectorType
 	switch tmp.SelectorType {
-	case DatabaseLabelDatabaseSubSelectorType:
-		c.Selector = &DatabaseLabelBasedSubSelector{}
+	case DatabaseLabelsDatabaseSubSelectorType:
+		c.Selector = &DatabaseLabelsBasedSubSelector{}
 	default:
 		return fmt.Errorf("received unknown sub-selector type: %s", tmp.SelectorType)
 	}
@@ -130,4 +103,40 @@ func (c *DatabaseBasedResourceSubSelectorContainer) UnmarshalJSON(data []byte) e
 	}
 
 	return nil
+}
+
+var _ SecurityPolicyRuleResourceSelector = &DatabaseBasedResourceSelector{}
+
+type DatabaseBasedResourceSelector struct {
+	Selectors []DatabaseBasedResourceSubSelectorContainer `json:"selectors"`
+}
+
+func (s *DatabaseBasedResourceSelector) ResourceSelectorType() ResourceSelectorType {
+	return DatabaseBasedResourceSelectorType
+}
+
+func (s *DatabaseBasedResourceSelector) ToResourceMap() map[string]any {
+	m := make(map[string]any, 1)
+
+	databasesArr := make([]any, 1)
+	subSelectorsMap := make(map[string]any, 3)
+	databasesArr[0] = subSelectorsMap
+
+	databaseLabelsArr := make([]any, 0, 1)
+
+	for _, subSelector := range s.Selectors {
+		switch subSelector.SelectorType {
+		case DatabaseLabelsDatabaseSubSelectorType:
+			databaseLabelsArr = append(databaseLabelsArr, subSelector.Selector.ToResourceMap())
+		}
+	}
+
+	subSelectorsMap[attributes.LabelSelectors] = databaseLabelsArr
+	m[attributes.Databases] = databasesArr
+	return m
+}
+
+type DatabaseBasedResourceSubSelector interface {
+	DatabaseBasedResourceSubSelectorType() DatabaseBasedResourceSubSelectorType
+	ToResourceMap() map[string]any
 }
