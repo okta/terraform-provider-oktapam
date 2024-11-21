254ecb5c49e9408541ddd8cccbd9933cae68f66c
JAmes Atwill <james.atwill@okta.com>
2024-11-21 12:39:09 -0800

    - terraform fmt

--- oktapam/testdata/individual_server_account_selector.tf
+++ oktapam/testdata/individual_server_account_selector.tf
@@ -1,6 +1,6 @@
 resource "oktapam_security_policy_v2" "individual_server_account_policy" {
-  name   = "test individual server account selector"
-  active = true
+  name       = "test individual server account selector"
+  active     = true
   principals = { user_groups = ["user_group_1", "user_group_2"] }
   rules = [
     {
