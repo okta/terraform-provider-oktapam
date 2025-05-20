76d8134882c74abaeebbfac7f6d6deb53d8c6bf3
Shrirang Adgaonkar <shrirang.adgaonkar@okta.com>
2025-05-19 19:39:26 -0700

    clean up tf for ciem (commit 1 )

--- oktapam/constants/descriptions/links.go
+++ oktapam/constants/descriptions/links.go
@@ -23,6 +23,5 @@ const (
 	LinkSecretFolder                       = "For details, see [Secret Folders](https://help.okta.com/okta_help.htm?type=oie&id=ext-pam-secrets-folder)"
 	LinkSecret                             = "For details, see [Secrets](https://help.okta.com/okta_help.htm?type=oie&id=ext-pam-secrets)"
 	LinkSecurityPolicy                     = "For details, see [Security policy](https://help.okta.com/okta_help.htm?type=oie&id=ext-pam-policy)."
-	LinkCloudConnection                    = "For details, see [Cloud Connection](https://help.okta.com/okta_help.htm?type=oie&id=ext-pam-entitlement-aws-connect)."
 	LinkSudoCommandsBundle                 = "For details, see [Sudo Commands Bundle](https://help.okta.com/okta_help.htm?type=oie&id=csh-pam-sudo-commands)."
 )
